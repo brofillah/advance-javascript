8f3dcc081cba1b8f4daa5b069d61eeb622bf962d
Your Name <you@example.com>
2019-09-06 10:52:19 +0700

    constructur function (yg biasa digunakan)

--- script.js
+++ script.js
@@ -19,26 +19,43 @@ let mahasiswa2 = {
 }
 
 // 2. Function Declaration 
-function Mahasiswa(nama, energi) {
-   let mahasiswa = {}
-   mahasiswa.nama = nama
-   mahasiswa.energi = energi
+// function Mahasiswa(nama, energi) {
+//    let mahasiswa = {}
+//    mahasiswa.nama = nama
+//    mahasiswa.energi = energi
    
-   mahasiswa.makan = function (porsi) {
-       this.energi += porsi
-       console.log(`Halo ${this.nama}, selamat makan!`);
-   }
+//    mahasiswa.makan = function (porsi) {
+//        this.energi += porsi
+//        console.log(`Halo ${this.nama}, selamat makan!`);
+//    }
 
-   mahasiswa.main = function (jam) {
-       this.energi -= jam
-       console.log(`Halo ${this.nama}, selamat bermain!`)
-   }
+//    mahasiswa.main = function (jam) {
+//        this.energi -= jam
+//        console.log(`Halo ${this.nama}, selamat bermain!`)
+//    }
 
-   return mahasiswa
-}
+//    return mahasiswa
+// }
 
-let farhan = Mahasiswa('Farhan', 10)
-let Doddy = Mahasiswa('Doddy', 20)
+// let farhan = Mahasiswa('Farhan', 10)
+// let Doddy = Mahasiswa('Doddy', 20)
 
-// 3. Constructor Function 
+// 3. Constructor Function
+function Mahasiswa (nama, energi) {
+    this.nama = nama
+    this.energi= energi
+
+    this.makan = function (porsi) {
+        this.energi += porsi
+        console.log(`Halo ${this.nama}, selamat makan!`);
+    }
+
+    this.main = function (jam) {
+        this.energi -= jam
+        console.log(`Halo ${this.nama}, selamat bermain!`);
+    }
+} 
+
+let farhan = new Mahasiswa('Farhan', 3)
+
 // 4. Object.create
